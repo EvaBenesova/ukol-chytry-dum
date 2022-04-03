08e1871a1732021558246d62cea40ec37ab0e872
EvaBenesova <ebenesova112@seznam.cz>
2022-04-03 18:54:38 +0200

    header and basic style work

--- src/index.jsx
+++ src/index.jsx
@@ -2,10 +2,15 @@ import React from 'react';
 import { render } from 'react-dom';
 import './style.css';
 
-const App = () => (
-  <>
+import smartHomeData from './smartHomeData';
+import Header from './components/Header/Header';
 
-  </>
-);
+const App = () => {
+  return(
+    <div className='container'>
+      <Header />
+    </div>
+  )
+};
 
 render(<App />, document.querySelector('#app'));
